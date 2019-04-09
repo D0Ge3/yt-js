7b2b268d8f62ce4f61a77957ed7461990e6508a7
D0Ge3 <36801043+D0Ge3@users.noreply.github.com>
2019-04-09 21:48:39 +0300

    Second homework
    Second homework (require fix bag with "Night mode")

--- js/script.js
+++ js/script.js
@@ -98,6 +98,85 @@ more.addEventListener('click', () => {
         setTimeout(() => {
             card.classList.remove( 'videos__item-active');
         }, 10);
+        bindNewModal(card);
 
     }
-})
+    
+    sliceTitle('.videos__item-descr', 100);
+    
+});
+
+function sliceTitle(selector, count) {
+    document.querySelectorAll(selector).forEach(item => {
+        item.textContent.trim();
+        
+        if(item.textContent.length < count) {
+            return;
+        } else {
+            const str = item.textContent.slice(0, count + 1) + "...";
+            item.textContent = str;
+        }
+    });
+}
+sliceTitle('.videos__item-descr', 100);
+
+function openModal() {
+    modal.style.display = 'block';
+}
+
+function closeModal() {
+    modal.style.display = 'none';
+    player.stopVideo();
+}
+
+function bindModay(cards) {
+    cards.forEach(item => {
+        item.addEventListener('click', (e) => {
+            e.preventDefault();
+            const id = item.getAttribute('data-url');
+            loadVideo(id);
+            openModal();
+        }); 
+    });
+}
+
+bindModay(videos);
+
+function bindNewModal(cards) {
+    cards.addEventListener('click', (e) => {
+        e.preventDefault();
+        const id = cards.getAttribute('data-url');
+        loadVideo(id);
+        openModal();
+    });
+}
+
+modal.addEventListener('click', (e) => {
+    if (!e.target.classList.contains('modal__body')) {
+        closeModal();
+   }
+});
+
+function createVideo() {
+    var tag = document.createElement('script');
+
+      tag.src = "https://www.youtube.com/iframe_api";
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+      
+      setTimeout(() => {
+        player = new YT.Player('frame', {
+            height: '100%',
+            width: '100%',
+            videoId: 'M7lc1UVf-VE'
+          });
+    }, 300);
+      
+}
+
+createVideo();
+
+function loadVideo(id) {
+    player.loadVideoById({
+        'videoId': `${id}`});
+}
